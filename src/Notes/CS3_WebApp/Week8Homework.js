863c60c02dfbf2ef834db775dad63967039600ec
MarkChenYutian <markchenyutian@gmail.com>
2020-12-17 19:08:28 +0800

    update Tic Tac Toe Game

--- src/Notes/CS3_WebApp/Week8Homework.js
+++ src/Notes/CS3_WebApp/Week8Homework.js
@@ -3,13 +3,13 @@ import '../../App.css';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
 import '../../index.css';
-import { Layout, Typography, Button, Space } from 'antd';
+import { Layout, Typography, Button, Space, InputNumber, Slider } from 'antd';
 import './Week8Homework.css';
 import AppHeader from '../../PublicComponent/Header';
 import AppFooter from '../../PublicComponent/Footer';
 import AppPageHeader from '../../PublicComponent/PageHeader';
 const { Content } = Layout;
-const { Title } = Typography;
+const { Title, Text, Paragraph } = Typography;
 function CS3Week8HW(){
    return(
        <Layout>
@@ -35,6 +35,10 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
   renderSquare(i) {
     return (
       <Square
@@ -44,24 +48,22 @@ class Board extends React.Component {
     );
   }
 
+  renderRow(leftNum) {
+    let row = Array();
+    for (let i = 0; i < this.props.size; i ++){
+      row.push(this.renderSquare(leftNum + i));
+    }
+    return row
+  }
+
   render() {
+    let board = Array();
+    for (let i = 0; i < this.props.size; i ++){
+      board.push(<div clsssName="board-row">{this.renderRow(0 + i * this.props.size)}</div>)
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
   }
@@ -71,9 +73,11 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      width: window.innerWidth,
+      size: 5,
       history: [
         {
-          squares: Array(9).fill(null)
+          squares: Array(25).fill(null)
         }
       ],
       stepNumber: 0,
@@ -85,7 +89,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares, this.state.size) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -107,10 +111,51 @@ class Game extends React.Component {
     });
   }
 
+  resetHandle(){
+    let mySize = this.state.size;
+    this.setState({
+      stepNumber: 0,
+      history: [
+        {
+          squares: Array(mySize * mySize).fill(null)
+        }
+      ],
+      xIsNext: true
+    });
+  }
+
+  setSize(newSize){
+      this.setState({
+        size: newSize,
+        stepNumber: 0,
+        history: [
+          {
+            squares: Array(newSize * newSize).fill(null)
+          }
+        ],
+        xIsNext: true
+      });
+  }
+
+  updateWidth = () =>{
+    this.setState({width: window.innerWidth})
+  }
+
+  componentDidMount(){
+    window.addEventListener('resize', this.updateWidth);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.updateWidth);
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares, this.state.size);
+
+    const width = Math.floor(((this.state.width * 0.5) - 150) / 50);
+    console.log("Board Max Width" + width);
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -129,9 +174,26 @@ class Game extends React.Component {
     }
 
     return (
+      <body>
+      <div>
+          <Title>Tic-tac-toe Game</Title>
+          <Text>Board Size:  </Text><Slider min={5} max={width} defaultValue={5} size="small" onChange={(value) => this.setSize(value)} style={{width: "300px"}}/>
+          <Button onClick={()=>this.resetHandle()} type="primary">Reset Board</Button>
+          <div style={{height: "20px"}}></div>
+          <Paragraph>
+          The upperbound of Board size is setup in a dynamic way, according to the window's height. In this case, the the board will not overflow.
+          </Paragraph>
+          <Paragraph>
+          Notice that I use an event listner to detect the change of Window Size to prevent Board Overflow. 
+          </Paragraph>
+          <Paragraph type="danger">
+          If you change the size of window when playing, ALL RECORDS WILL BE WIPED AWAY!
+          </Paragraph>
+      </div>
       <div className="game">
         <div className="game-board">
           <Board
+            size={this.state.size}
             squares={current.squares}
             onClick={i => this.handleClick(i)}
           />
@@ -141,28 +203,114 @@ class Game extends React.Component {
           <Space direction="vertical">{moves}</Space>
         </div>
       </div>
+      </body>
     );
   }
 }
 
 // ========================================
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+function calculateWinner(squares, size) {
+  for (let index = 0; index < squares.length; index ++){
+    let hRes = checkHorizontalLine(index, size, squares);
+    if (hRes != null){
+      return hRes;
     }
+    let vRes = checkVerticalLine(index, size, squares);
+    if (vRes != null){
+      return vRes;
+    }
+    let LRDRes = checkLRDiagnal(index, size, squares);
+    if (LRDRes != null){
+      return LRDRes;
+    }
+    let RLDRes = checkRLDiagnal(index, size, squares);
+    if (RLDRes != null){
+      return RLDRes;
+    }
+  }
+  return null;
+}
+
+/*
+0 0 0 0 0
+0 0 0 0 0
+1 1 1 1 1
+0 0 0 0 0
+0 0 0 0 0
+*/
+function checkHorizontalLine(currIndex, size, squares){
+  if (Math.floor((currIndex - 2) / size) === Math.floor(currIndex / size) &&
+      Math.floor((currIndex + 2) / size) === Math.floor(currIndex / size)){
+      if (squares[currIndex] === squares[currIndex - 1] &&
+          squares[currIndex] === squares[currIndex - 2] && 
+          squares[currIndex] === squares[currIndex + 1] &&
+          squares[currIndex] === squares[currIndex + 2]){
+        return squares[currIndex];
+      }
+  }
+  return null
+}
+
+/*
+0 0 1 0 0
+0 0 1 0 0
+0 0 1 0 0
+0 0 1 0 0
+0 0 1 0 0
+*/
+function checkVerticalLine(currIndex, size, squares){
+  if (currIndex - 2 * size > -1 && currIndex + 2 * size < squares.length){
+    if (squares[currIndex] === squares[currIndex - 1 * size] &&
+      squares[currIndex] === squares[currIndex - 2 * size] && 
+      squares[currIndex] === squares[currIndex + 1 * size] &&
+      squares[currIndex] === squares[currIndex + 2 * size]){
+    return squares[currIndex];
+  }
+  }
+  return null;
+}
+
+/*
+1 0 0 0 0
+0 1 0 0 0
+0 0 1 0 0
+0 0 0 1 0
+0 0 0 0 1
+*/
+function checkLRDiagnal(currIndex, size, squares){
+  if (Math.floor((currIndex - 2 * size) / size) === Math.floor((currIndex - 2 - 2 * size) / size) &&
+      Math.floor((currIndex + 2 * size) / size) === Math.floor((currIndex + 2 + 2 * size) / size) &&
+      currIndex + 2 * size < squares.length &&
+      currIndex - 2 * size > -1){
+        if (squares[currIndex] === squares[currIndex - 1 * size - 1] &&
+          squares[currIndex] === squares[currIndex - 2 * size - 2] && 
+          squares[currIndex] === squares[currIndex + 1 * size + 1] &&
+          squares[currIndex] === squares[currIndex + 2 * size + 2]){
+            return squares[currIndex];
+        }
+  }
+  return null;
+}
+
+/*
+0 0 0 0 1
+0 0 0 1 0
+0 0 1 0 0
+0 1 0 0 0
+1 0 0 0 0
+*/
+function checkRLDiagnal(currIndex, size, squares){
+  if (Math.floor((currIndex - 2 * size) / size) === Math.floor((currIndex + 2 - (2 * size)) / size) &&
+      Math.floor((currIndex + 2 * size) / size) === Math.floor((currIndex - 2 + (2 * size)) / size) &&
+      currIndex + 2 * size < squares.length &&
+      currIndex - 2 * size > -1){
+        if (squares[currIndex] === squares[currIndex - 1 * size + 1] &&
+          squares[currIndex] === squares[currIndex - 2 * size + 2] && 
+          squares[currIndex] === squares[currIndex + 1 * size - 1] &&
+          squares[currIndex] === squares[currIndex + 2 * size - 2]){
+            return squares[currIndex];
+        }
   }
   return null;
 }
